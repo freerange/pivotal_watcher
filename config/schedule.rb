2fb4c708a9897381678ffcd10d118f7fa80a098d
James Mead <james@floehopper.org>
2014-02-14 10:54:23 +0000

    Disable emails to FutureLearn.
    My FutureLearn Pivotal Tracker account has been disabled and this is now
    sending emails containing stack traces to FutureLearn.

--- config/schedule.rb
+++ config/schedule.rb
@@ -25,5 +25,5 @@ env :PIVOTAL_WATCHER_RECIPIENTS, 'product@futurelearn.com'
 
 every :weekday, at: '7am' do
   # Write the script output to a file and then, if the file has a size greater than 0, mail the content of the file to everyone
-  command 'cd ~/app && rm -f tmp-script-output && bundle exec ruby watch.rb > tmp-script-output 2>&1; if [ -s tmp-script-output ]; then cat tmp-script-output | mail -s "Pivotal Tracker Watcher Report" $PIVOTAL_WATCHER_RECIPIENTS; fi'
+  # command 'cd ~/app && rm -f tmp-script-output && bundle exec ruby watch.rb > tmp-script-output 2>&1; if [ -s tmp-script-output ]; then cat tmp-script-output | mail -s "Pivotal Tracker Watcher Report" $PIVOTAL_WATCHER_RECIPIENTS; fi'
 end
